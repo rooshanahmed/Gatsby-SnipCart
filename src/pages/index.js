e8bc21c4f44972f26f0616a3a1be8b7e35aba6bb
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2020-12-31 14:12:55 +0500

    index.js updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button
+        class="snipcart-add-item"
+        data-item-id="formal-shoe"
+        data-item-price="30"
+        data-item-description="High-Qulality Shoe in best price"
+        data-item-url="/"
+        data-item-image="https://media.thecustommovement.com/308274c7-efad-4f60-910b-fe1d7851d40c"
+        data-item-name="The Starry Night"
+      >
+        Checkout
+      </button>
+    </div>
+  )
 }
